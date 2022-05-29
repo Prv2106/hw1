21a095f63ff31a1f25494f082566dce5db648681
Alberto Provenzano <101663958+Prv2106@users.noreply.github.com>
2022-05-29 17:24:47 +0200

    Update home.php

--- home.php
+++ home.php
@@ -78,11 +78,11 @@
                 <h1>
                     <div>Ciao
                         <?php
-                            $query_name = "SELECT nome from users where BINARY username = '".$username."' ";
+                            $query_name = "SELECT name from users where BINARY username = '".$username."' ";
                             $name = mysqli_query($conn,$query_name);
                             if(mysqli_num_rows($name) > 0){
                                 $row = mysqli_fetch_assoc($name);
-                                echo $row['nome'];
+                                echo $row['name'];
                             }
                         ?>
                     </div> 
